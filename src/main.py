3c741aeca362442bda62cbc6c24a29566cbfb118
Sophia <sophiajones@live.co.uk>
2025-03-28 12:13:06 +0000

    add torch device

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
+import torch
 import yaml
+import torch
 from argparse import ArgumentParser
 from agents.ppo import PPO
 
 
 def main(hyperparams):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     # Load YAML hyperparameters
-    with open(f'./src/hyperparam/{hyperparams.config_file}', 'r') as f:
+    with open(f'utils/hyperparameters.yaml', 'r') as f:
         hparam = yaml.safe_load(f)
 
     # Replace hparams with command line arguments
@@ -13,20 +18,19 @@ def main(hyperparams):
         if v is not None:
             hparam[k] = v
 
-    PPO(**hparam)
+    # set seeds
+    PPO(device, **hparam)
 
 
 if __name__ == '__main__':
 
     parser = ArgumentParser()
-
     parser.add_argument(
         '--save_dir',
         type=str,
         default=None,
         help='Directory to save the model.'
     )
-
     hparam = parser.parse_args()
 
     main(hparam)
