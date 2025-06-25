b0c301ebbe71c19e447fe8c2b43c16b50d164f01
Sophia <sophiajones@live.co.uk>
2025-06-25 11:14:14 +0100

    run code for regularization plot on hex

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import os
 import torch
+import multiprocessing as mp
 import sys
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, project_root)
-import multiprocessing as mp
-from src.utils import load_hparams, load_agent_and_env, set_run_id, get_logger
+from src.utils import load_hparams, load_agent_and_env, set_run_id, get_logger, plot_rewards_with_seeds
 from src.agents.ppo import PPO
-from src.utils.analyse_data import plot_rewards_with_seeds
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
@@ -25,9 +24,11 @@ if __name__ == '__main__':
     hparams = load_hparams(os.path.join('utils', 'hyperparameters.yaml'), num_seeds=5)
     if torch.cuda.is_available():
         mp.set_start_method('spawn', force=True)
-        pool = mp.Pool(processes=min(6, len(hparams)))
+        pool = mp.Pool(processes=min(4, len(hparams)))
         results = pool.map(run, hparams)
         pool.close()
         pool.join()
-    # plot_rewards_with_seeds(f'{hparams[0]["run_dir"]}/results')
-    # plot_rewards_with_seeds(f'../runs/best')
+    else:
+        run(hparams[0])
+    plot_rewards_with_seeds(f'{hparams[0]["run_dir"]}/results')
+    # plot_rewards_with_seeds(f'../runs/change ppo, ftn_approx/results')
