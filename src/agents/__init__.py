6fc0bd2cfa329f334f7b4edc189ef5cadc38cbf1
Sophia <sophiajones@live.co.uk>
2025-08-11 17:23:42 +0100

    clean up

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,5 +1,6 @@
-from .method1 import Method1Gnn, SkrlMethod1
-from .method2 import Method2Gnn, SkrlMethod2
+from .method1 import Method1Gnn
+from .method2 import Method2Gnn
 # from .method5 import Method5Gnn
-from .nerve_net import MessagePassingGNN as NerveNet, SkrlNerveNet
-from .feed_forward import FeedForward, SKRLFeedForward
+from .nerve_net import MessagePassingGNN as NerveNet
+from .feed_forward import FeedForward
+from .ppo import PPO
