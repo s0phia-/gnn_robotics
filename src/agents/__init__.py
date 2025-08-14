e9ec64e94633ba4bb0351f32e042301bf9bdf885
Sophia <sophiajones@live.co.uk>
2025-08-14 16:19:54 +0100

    add agents to init file

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,7 +1,7 @@
 from .method1 import Method1Gnn
 from .method2 import Method2Gnn
 from .nerve_net import MessagePassingGNN as NerveNet
-from .EGAT import EGATMethod
-from .GAT import GAT_Method
+from .EGAT import EGATMethod as EGAT
+from .GAT import GAT_Method as GAT
 from .feed_forward import FeedForward
 from .ppo import PPO
